c08fbfffea3a6eefa5e3630c3a6e2fdb7ce7cb3c
Abdou Aziz Ndiaye <abdouaziznjay@gmail.com>
2025-03-17 12:57:39 +0000

    refactor(board): make the board adapt to the height of the grid

--- lib/ui/widgets/board.dart
+++ lib/ui/widgets/board.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swipe_detector/flutter_swipe_detector.dart';
 import 'package:provider/provider.dart';
 import 'package:twenty_forty_eight/constants/colorscheme.dart';
+import 'package:twenty_forty_eight/models/game_board.dart';
 import 'package:twenty_forty_eight/providers/game_provider.dart';
 import 'package:twenty_forty_eight/ui/widgets/tile.dart';
 
@@ -21,32 +22,40 @@ class _BoardState extends State<Board> {
   @override
   Widget build(BuildContext context) {
     final gameProvider = Provider.of<GameProvider>(context);
+    final boardSize = gameProvider.gameBoard.size; // Number of rows/columns
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    final tileSize = (screenWidth < 500 ? screenWidth - 40 : 600) / boardSize;
+
+    final boardHeight = tileSize * boardSize;
+
     return LayoutBuilder(builder: (context, constraints) {
       return SwipeDetector(
-          onSwipeUp: (offset) => gameProvider.swipe(SwipeDirection.up),
-          onSwipeDown: (offset) => gameProvider.swipe(SwipeDirection.down),
-          onSwipeLeft: (offset) => gameProvider.swipe(SwipeDirection.left),
-          onSwipeRight: (offset) => gameProvider.swipe(SwipeDirection.right),
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            height: MediaQuery.of(context).size.width < 500 ? 500 : 600,
-            width: MediaQuery.of(context).size.width < 500
-                ? MediaQuery.of(context).size.width - 40
-                : 600,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(12)),
-                color: boardColor),
-            child: GridView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: gameProvider.gameBoard.grid.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: gameProvider.gameBoard.size,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10),
-              itemBuilder: (context, index) =>
-                  Tile(value: gameProvider.gameBoard.grid[index].value),
+        onSwipeUp: (offset) => gameProvider.swipe(SwipeDirection.up),
+        onSwipeDown: (offset) => gameProvider.swipe(SwipeDirection.down),
+        onSwipeLeft: (offset) => gameProvider.swipe(SwipeDirection.left),
+        onSwipeRight: (offset) => gameProvider.swipe(SwipeDirection.right),
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          height: boardHeight, // Adaptive height
+          width: screenWidth < 500 ? screenWidth - 40 : 600,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+            color: boardColor,
+          ),
+          child: GridView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: gameProvider.gameBoard.grid.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: boardSize,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
             ),
-          ));
+            itemBuilder: (context, index) =>
+                Tile(value: gameProvider.gameBoard.grid[index].value),
+          ),
+        ),
+      );
     });
   }
 }
